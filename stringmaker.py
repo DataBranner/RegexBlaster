fa8a279cf57246b34452faa08c55ef1149d30b37
David Branner <databranner@gmail.com>
2014-03-30 14:37:14 -0400

    update stringmaker

--- stringmaker.py
+++ stringmaker.py
@@ -5,18 +5,27 @@
 import string
 import random
 
-def string_w_repeats(fill_w_space=False, fill_w_tab=False, fill_w_CR=False):
-    germ_stuff = string.ascii_lowercase
-    fill_stuff = string.ascii_uppercase
-    repeats = random.randint(2, 5)
-    string_length = random.randint(2, 5)
-    germ = ''.join([random.choice(germ_stuff) for i in range(string_length)])
-    string_length = random.randint(1, string_length+1)
-    string_w_repeats = ''
-    for i in range(repeats):
-        string_w_repeats += (germ + ''.join([random.choice(fill_stuff)
-            for i in range(string_length)]))
-    return string_w_repeats
+class StringMaker():
+    def __init__(self):
+        self.germ_w_space=False
+        self.germ_w_tab=False
+        self.germ_w_CR=False
+        self.fill_w_space=False
+        self.fill_w_tab=False
+        self.fill_w_CR=False
+
+    def string_w_repeats():
+        germ_stuff = string.ascii_lowercase
+        fill_stuff = string.ascii_uppercase
+        repeats = random.randint(2, 5)
+        string_length = random.randint(2, 5)
+        germ = ''.join([random.choice(germ_stuff) for i in range(string_length)])
+        string_length = random.randint(1, string_length+1)
+        string_w_repeats = ''
+        for i in range(repeats):
+            string_w_repeats += (germ + ''.join([random.choice(fill_stuff)
+                for i in range(string_length)]))
+        return string_w_repeats
 
 def string_w_spaces():
     pass
