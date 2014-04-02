e44995d176f8a5364eec190df5e41042dad4a1f4
David Branner <databranner@gmail.com>
2014-04-02 08:25:31 -0400

    add working curses_tester_03.py

--- curses_tester_03.py
+++ curses_tester_03.py
@@ -5,7 +5,8 @@
 
 """Explore the use of Ncurses.
 
-03 Set up different sub-windows.
+next: Set up different sub-windows.
+03 Get styled text working.
 02 Set up and close curses in special functions; always trap ctrl-c.
 01 Open window; close on two backticks.
 """
@@ -16,14 +17,24 @@ if sys.version_info[0] != 3:
     sys.exit()
 import curses
 import random
+import time
+import datetime
 
 def main():
     stdscr = set_up_curses()
     try:
         main_loop(stdscr)
     except KeyboardInterrupt:
-        close_curses(stdscr)
-    close_curses(stdscr)
+        # If program core finished, restore terminal settings.
+        curses.nocbreak() # end character-break mode.
+        stdscr.keypad(0)
+        curses.echo()
+        curses.curs_set(1)
+        # Destroy window.
+        curses.endwin()
+
+#        close_curses(stdscr)
+#    close_curses(stdscr)
 
 
 ###################
@@ -32,17 +43,29 @@ def main():
 
 def main_loop(stdscr):
     window = curses.newwin(curses.LINES, curses.COLS)
+    start_time = time.time()
     while True:
-        c = window.getch()
-        if c == ord('`'):
-            c = window.getch()
-            if c == ord('`'):
-                break
         # Different subwindows: score_bar, main_win, defense_bar.
-        score = 'Score: {:>4}'.format(random.randint(1, 100))
-        stdscr.addstr(0, 0, score, curses.A_REVERSE)
-        stdscr.chgat(-1, curses.A_REVERSE)
-        stdscr.chgat(0, 0, -1, curses.color_pair(130))
+        t = str(datetime.timedelta(seconds=round(time.time()-start_time)))
+        score = ('''Score: {:>4}  Level: {:>4}  Damage: {:>3}  '''
+                '''Time remaining: {}'''.
+                        format(random.randint(1, 100), random.randint(1, 100),
+                        random.randint(1, 100), t))
+        stdscr.addstr(0, 0, score)#, curses.A_REVERSE)
+#        stdscr.chgat(-1, curses.A_REVERSE)
+        stdscr.chgat(0, 0, 7, curses.color_pair(142))
+        stdscr.chgat(0, 12, 7, curses.color_pair(142))
+        stdscr.chgat(0, 24, 9, curses.color_pair(142))
+        stdscr.chgat(0, 36, 17, curses.color_pair(142))
+        stdscr.chgat(0, 7, 4, curses.color_pair(198))
+        stdscr.chgat(0, 20, 4, curses.color_pair(198))
+        stdscr.chgat(0, 34, 3, curses.color_pair(198))
+        stdscr.chgat(0, 53, 10, curses.color_pair(198))
+        # Get next character in regex string.
+        c = window.getch()
+        # Append to regex string and try against attack and non-combattant
+        # strings.
+        pass
         refresh(stdscr, window)
 
 # Things to do:
