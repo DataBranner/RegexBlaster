735a0bc9990f6af529550360390a3397f8db082a
David Branner <databranner@gmail.com>
2014-04-08 14:28:25 -0400

    change timing to counter with limit optional

--- regex_blaster_11.py
+++ regex_blaster_11.py
@@ -13,14 +13,21 @@ import sys
 import curses
 import random
 import string
+import traceback
 from cursesdisplay import CursesDisplay
+from timer import Timer
+from scorer import Scorer
+
+cd = CursesDisplay()
+attack_limit = cd.attacks_max
+T = Timer()
+S = Scorer(attack_limit)
 
 def main():
-    cd = CursesDisplay()
     try:
         main_loop(cd)
     except KeyboardInterrupt:
-        # If program core finished, restore terminal settings.
+        # If ctrl-c, restore terminal settings.
         curses.nocbreak() # end character-break mode.
         cd.stdscr.keypad(0)
         curses.echo()
@@ -33,14 +40,22 @@ def main():
 ###################
 
 def main_loop(cd):
-    while cd.S.damage > 0:
-        curses.delay_output(10)
-        cd.display_score()
-        cd.display_message()
-        cd.display_defense()
-        attack_defend_cycle(cd)
-    if cd.S.damage <= 0:
-        cd.S.message = 'Your player has been destroyed in battle. Game over.'
+    while S.attack_limit > 0:
+        if T.time_limit and T.time_to_display < 0:
+            cd.end_game()
+        else:
+            T.update()
+            curses.delay_output(10)
+            cd.display_score(S.score, T.time_to_display_str, S.attack_limit)
+            cd.display_message(S.message)
+            cd.display_defense(S.defense)
+            attack_defend_cycle(cd)
+    if S.attack_limit <= 0:
+        S.message = ('''Your player has been destroyed in battle. '''
+                '''Game over; ctrl-c to quit.''')
+        cd.display_message(S.message)
+        while True:
+            pass
 
 #######################
 # End of program body #
@@ -48,15 +63,15 @@ def main_loop(cd):
 
 def attack_defend_cycle(cd):
     # Generate "attack" string (must be matched to avoid hit)
-    if cd.S.new_attacks:
-        cd.S.new_attacks = False
-        cd.S.attack = generate_string()
-        cd.display_attacks()
+    if S.new_attacks:
+        S.new_attacks = False
+        S.attack = generate_string()
+        cd.display_attacks(S.attack)
     # Generate "noncombatant" string (must be matched to avoid score-loss)
-    if cd.S.new_noncomb:
-        cd.S.new_noncomb = False
-        cd.S.noncombatant = generate_string()
-        cd.display_noncomb()
+    if S.new_noncomb:
+        S.new_noncomb = False
+        S.noncombatant = generate_string()
+        cd.display_noncomb(S.noncombatant)
     #
     # Battle state loop.
     # Get next character in regex string.
@@ -64,19 +79,19 @@ def attack_defend_cycle(cd):
         c = cd.window.getch()
         # Delete last character: DEL, BS.
         if c in {127, 18}:
-            cd.S.defense = cd.S.defense[:-1]
+            S.defense = S.defense[:-1]
         # Submit finished string: CR, LF, VT, FF, ESC.
         elif c in {10, 13, 11, 12, 27}:
             # Clear message and submit handle completed defense string.
-            cd.S.message = ''
-            cd.S.defense_submitted = cd.S.defense
+            S.message = ''
+            S.defense_submitted = S.defense
         # Other control characters. QQQ we have not dealt with arrow keys.
         elif c == -1 or not (32 <= c <= 126):
             pass
-        # For checking other delete characters, use cd.S.defense += str(c)
+        # For checking other delete characters, use S.defense += str(c)
         else:
-#            cd.S.defense += str(c)
-            cd.S.defense += chr(c)
+#            S.defense += str(c)
+            S.defense += chr(c)
     except ValueError:
         pass
     # Append to regex string and try against attack and non-combatant strings.
@@ -84,31 +99,31 @@ def attack_defend_cycle(cd):
     #    Finished defense string is passed to assess_defense_single; 
     #    Unfinished defense string is, if possible, evaluated tentatively 
     #        against attack and noncombatant strings.
-    if cd.S.defense_submitted:
+    if S.defense_submitted:
         # Check defense against past regexes; invalidate if found.
-        if cd.S.defense_submitted in cd.S.defense_record:
-            cd.S.message = 'This defense has already been used; invalid.'
+        if S.defense_submitted in S.defense_record:
+            S.message = 'This defense has already been used; invalid.'
         else:
-            cd.S.new_attacks = True
-            cd.S.new_noncomb = True
-            cd.S.defense_record.add(cd.S.defense_submitted)
+            S.new_attacks = True
+            S.new_noncomb = True
+            S.defense_record.add(S.defense_submitted)
             # Evaluate defense.
-            cd.S.assess_defense_single()
-            cd.S.defense_submitted = ''
+            S.assess_defense_single()
+            S.defense_submitted = ''
             # Act on attack_successful, collateral_damage
-            cd.S.score_defense()
-            cd.S.defense = ''
-            if cd.S.attack_successful and not cd.S.collateral_damage:
+            S.score_defense()
+            S.defense = ''
+            if S.attack_successful and not S.collateral_damage:
                 # Fade attack. 
                 cd.fade_out(
                         cd.attacks, cd.attacks_row, 1, cd.half_screen-2)
                 if cd.attacks_row > 2:
                     cd.attacks_row -= 1
                 cd.noncomb_row -= 1
-            elif cd.S.collateral_damage:
+            elif S.collateral_damage:
                 cd.highlight_failure(
                         cd.noncomb, cd.noncomb_row, 1, cd.half_screen-2)
-            elif not cd.S.attack_successful:
+            elif not S.attack_successful:
                 cd.highlight_failure(
                         cd.attacks, cd.attacks_row, 1, cd.half_screen-2)
 
